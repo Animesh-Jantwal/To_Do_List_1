50005e4cfa14042f19ba975404c679b7b1073914
Animesh-Jantwal <animeshjantwalcoding@gmail.com>
2025-08-19 18:45:53 +0530

    Fixed few issues and added click functiuonality

--- script.js
+++ script.js
@@ -9,8 +9,16 @@ function addTask() {
         li.innerHTML = inputBox.value;
         List.appendChild(li);
         let span = document.createElement("span");
-        span.innerHTML = "\u007d";
+        span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
     inputBox.value = "";
 }
+
+List.addEventListener ("click" , function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+} , false)
